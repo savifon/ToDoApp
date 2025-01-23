5013a658f5ff5c2ab307d8e559c8e7c3c9798d29
savio.fonseca <savio.fonseca@safelabs.com.br>
2025-01-23 09:15:24 -0300

    read and create endpoints

---
+++ main.py
@@ -0,0 +1,52 @@
+from typing import Annotated
+
+from fastapi import FastAPI, HTTPException, Path, Depends
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from starlette import status
+
+import models
+from models import ToDos
+from database import SessionLocal, engine
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class ToDoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(ToDos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found.')
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: ToDoRequest):
+    todo_model = ToDos(**todo_request.__dict__)
+
+    db.add(todo_model)
+    db.commit()
