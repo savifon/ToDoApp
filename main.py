ad2eaee24902fb226029e44987ddd2a8d553cdab
savio.fonseca <savio.fonseca@safelabs.com.br>
2025-01-23 10:28:52 -0300

    delete endpoint

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
     if todo_model is not None:
         return todo_model
-    raise HTTPException(status_code=404, detail='Todo not found.')
+    raise HTTPException(status_code=404, detail='ToDo not found.')
 
 
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
@@ -58,7 +58,7 @@ async def update_todo(db: db_dependency,
                       todo_id: int = Path(gt=0)):
     todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
     if todo_model is None:
-        raise HTTPException(status_code=404, detail='Todo not found.')
+        raise HTTPException(status_code=404, detail='ToDo not found.')
 
     todo_model.title = todo_request.title
     todo_model.description = todo_request.description
@@ -66,4 +66,14 @@ async def update_todo(db: db_dependency,
     todo_model.complete = todo_request.complete
 
     db.add(todo_model)
+    db.commit()
+
+
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='ToDo not found.')
+    db.query(ToDos).filter(ToDos.id == todo_id).delete()
+
     db.commit()
