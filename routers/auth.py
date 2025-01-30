fa18e22a3bb9078eef44df73c6c9ae43e5c00a58
savio.fonseca <savio.fonseca@safelabs.com.br>
2025-01-30 11:44:18 -0300

    saving the user in the database

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,11 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
 
@@ -17,8 +23,20 @@ class CreateUserRequest(BaseModel):
     role: str
 
 
-@router.post("/auth/")
-async def create_user(create_user_request: CreateUserRequest):
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+                      create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -28,4 +46,6 @@ async def create_user(create_user_request: CreateUserRequest):
         hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active= True
     )
-    return create_user_model
+
+    db.add(create_user_model)
+    db.commit()
