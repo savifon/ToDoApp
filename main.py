d49782249ce6c7278e9b9035eb42b276b7450da0
Savio Rodrigues <savio.fonseca@safelabs.com.br>
2025-04-30 16:41:00 -0300

    adding initial jinja template

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 from routers import auth, todos, admin, users
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 @app.get("/healthy")
 def health_check():
